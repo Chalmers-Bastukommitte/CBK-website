9fc565393497e00aca879d31da3b601f2a75c9eb
Huml4n <h.forsling@gmail.com>
2024-08-27 14:20:26 +0200

    start

---
+++ src/components/Features/featuresData.tsx
@@ -0,0 +1,71 @@
+import { Feature } from "@/types/feature";
+
+const featuresData: Feature[] = [
+  {
+    id: 1,
+    icon: (
+      <img
+      src={"https://cbk.chs.chalmers.se/wp-content/uploads/2023/08/Yxa-scaled-e1699394302653-640x640.jpg"}/>
+    ),
+    title: 
+      "RFSU - Mattias Hertzberg",
+    paragraph:
+      "Tuae nam ex similique incidunt expedita exerci tationem laudantium. Repellendus quisquam numquam perferendis earum sapiente non tempore? Fugit repellat ut maiores.",
+  },
+  {
+    id: 1,
+    icon: (
+      <img
+      src={"https://cbk.chs.chalmers.se/wp-content/uploads/2024/03/1000009255-scaled-e1722528486360-640x640.jpg"}/>
+    ),
+    title: 
+      "Bärra - Love Lindqvist",
+    paragraph:
+      "Tuae nam ex similique incidunt expedita exerci tationem laudantium. Repellendus quisquam numquam perferendis earum sapiente non tempore? Fugit repellat ut maiores.",
+  },
+  {
+    id: 1,
+    icon: (
+      <img
+      src={"https://cbk.chs.chalmers.se/wp-content/uploads/2023/08/Patron-1-scaled-e1699470840477-640x640.jpg"}/>
+    ),
+    title: 
+      "Patrôn - Daniel Hellberg",
+    paragraph:
+      "Tuae nam ex similique incidunt expedita exerci tationem laudantium. Repellendus quisquam numquam perferendis earum sapiente non tempore? Fugit repellat ut maiores.",
+  },
+  {
+    id: 1,
+    icon: (
+      <img
+      src={""}/>
+    ),
+    title: 
+      "PÒ - Wanda Wannelöf",
+    paragraph:
+      "Tuae nam ex similique incidunt expedita exerci tationem laudantium. Repellendus quisquam numquam perferendis earum sapiente non tempore? Fugit repellat ut maiores.",
+  },
+  {
+    id: 1,
+    icon: (
+      <img
+      src={"https://cbk.chs.chalmers.se/wp-content/uploads/2024/03/20240615_114735-scaled-e1722528523161-640x640.jpg"}/>
+    ),
+    title: 
+      "Volt - Simon Holmgren",
+    paragraph:
+      "Tuae nam ex similique incidunt expedita exerci tationem laudantium. Repellendus quisquam numquam perferendis earum sapiente non tempore? Fugit repellat ut maiores.",
+  },
+  {
+    id: 1,
+    icon: (
+      <img
+      src={"https://scontent-arn2-1.xx.fbcdn.net/v/t39.30808-6/456544329_2491693864375086_4440926337654526899_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=xmF8c0MB1icQ7kNvgFTJw2S&_nc_ht=scontent-arn2-1.xx&oh=00_AYBLec9WItUrZODdM4tDuL0hXOGmMONrr8BkJWr6yo93QA&oe=66D0004E"}/>
+    ),
+    title: 
+      "Skråt - Hanna Forsling",
+    paragraph:
+      "Tuae nam ex similique incidunt expedita exerci tationem laudantium. Repellendus quisquam numquam perferendis earum sapiente non tempore? Fugit repellat ut maiores.",
+  }
+];
+export default featuresData;
