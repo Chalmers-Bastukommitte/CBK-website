c4b3258b3e0e7f7731f6daffa729222c2c11ea89
Huml4n <h.forsling@gmail.com>
2024-09-25 16:23:13 +0200

    ready for gradient

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -6,18 +6,19 @@ const Hero = () => {
       <section
         id="home"
         className="relative z-10 bg-white pb-16 pt-[120px] dark:bg-gray-dark md:pb-[120px] md:pt-[240px] xl:pb-[160px] xl:pt-[320px] 2xl:pb-[200px] 2xl:pt-[400px]"
-        style={{backgroundImage: `url(${Background.src})`,
+        style={{
+          backgroundImage: `linear-gradient(to bottom, rgba(0, 15, 33, 0), rgba(0, 0, 0, 0)), url(${Background.src})`,
           backgroundRepeat: "no-repeat",
           backgroundSize: "cover",
         }}
         >
         <div className="container">
           <div className="-mx-4 flex flex-wrap">
-            <div className="w-full px-4">
-              <div className="mx-auto max-w-[800px] text-center">
-                <h1 className="mb-5 text-3xl font-bold leading-tight text-white dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
-                  
-                </h1>
+        <div className="w-full px-4">
+          <div className="mx-auto max-w-[800px] text-center">
+            <h1 className="mb-5 text-3xl font-bold leading-tight text-white dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
+          
+            </h1>
                 <p className="mb-12 text-base !leading-relaxed text-white dark:text-white sm:text-lg md:text-xl">
 
                 </p>
