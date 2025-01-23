3ad4fa6c06d1d745f232b202343e40a45595fca7
Louie-lasse <love.lindqvist101@gmail.com>
2025-01-23 11:22:29 +0100

    Fixing build issues

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,6 +4,7 @@ import { ThemeProvider } from "next-themes";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
+    // @ts-expect-error: TypeScript is complaining about `children`, but it works as expected
     <ThemeProvider attribute="class" enableSystem={false} defaultTheme="dark">
       {children}
     </ThemeProvider>
